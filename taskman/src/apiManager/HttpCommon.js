a070764da1bf584baa8b688d98d30d0e6b8577b2
Arun-Thampy <arunthampyec@gmail.com>
2024-09-29 10:00:33 +0530

    first commit

--- taskman/src/apiManager/HttpCommon.js
+++ taskman/src/apiManager/HttpCommon.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const getUrl = () => {
   //return window.location.href.split("#")[0] + "smartway";
-  return "https://smart-way-final-api-six.vercel.ap/smartway";
+  return "https://smart-way-final-api-six.vercel.app/smartway";
 };
 export default axios.create({
   baseURL: getUrl(),
